933d75c89605d0c4cb863e7367c557001e8fff60
Flávio Diniz de Sousa <dinizdesousaflavio@gmail.com>
2023-11-21 12:30:28 -0300

    Version 0.2

---
+++ src/js/menuGenerator.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Criação do elemento nav
+    var navElement = document.createElement("nav");
+    navElement.className = "menuBar";
+
+    // Conteúdo do nav
+    navElement.innerHTML = `
+    <div>
+    <a href="../../../index.html">
+        <strong>
+            <p class="menuHomeButton">Terra Loadout</p>
+        </strong>
+    </a>
+    </div>
+    <button class="menuBarButton">
+        <p>&#9776;</p>
+    </button>
+    <ul class="menuItens">
+        <li class="menuItem"><a href="../content/mage-class.html">
+            <p>Mago</p>
+        </a></li>
+        <li class="menuItem"><a href="../content/melee-class.html">
+            <p>Melee</p>
+        </a></li>
+        <li class="menuItem"><a href="../content/ranger-class.html">
+            <p>Atirador</p>
+        </a></li>
+        <li class="menuItem"><a href="../content/summoner-class.html">
+            <p>Invocador</p>
+        </a></li>
+        <li class="menuItem"><a href="../content/super-regen.html">
+            <p>Super Regen</p>
+        </a></li>
+    </ul>
+    `;
+
+    // Adiciona o elemento nav ao corpo da página
+    document.body.appendChild(navElement);
+
+    navElement.style.position = "fixed";
+    navElement.style.top = "0";
+
+    var mainContent = document.querySelector(".main-content"); // Selecione a classe ou id do seu conteúdo principal
+    if (mainContent) {
+        mainContent.style.marginTop = navElement.offsetHeight + "px";
+    }
+});
---
+++ src/js/menuScript.js
@@ -0,0 +1,4 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", () =>
+    document.querySelector(".container1").classList.toggle("show-menu"));
